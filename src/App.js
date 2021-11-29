313b30d3713ee53317966b632d42777cd62b4557
Juan Lucero <jlucero@alhpaworks.tech>
2021-11-28 17:04:45 -0800

    feat(header): add header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Header from './Components/Header';
@@ -11,9 +10,9 @@ function App() {
   const [listContainer, setListContainer] = useState([]);
   const [selectedItem, setSelectedItem] = useState('');
 
-
   return (
     <div id="decidr">
+      <Header />
       <main>
       {selectedItem.length === 0
         ? <div>
