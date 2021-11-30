238232e75c71f3af33e579dfd4b1405c2fcb24c0
Juan Lucero <jlucero@alhpaworks.tech>
2021-11-30 14:07:55 -0800

    chore(add item): add item to list container #^

---
+++ src/Components/AddItem.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function AddItem({ listContainer, setListContainer }) {
+  const [newItem, setNewItem] = useState('');
+
+  const formSubmit = (event) => {
+    event.preventDefault();
+    setListContainer([...listContainer, newItem]);
+    setNewItem('');
+  }
+
+  return (
+    <form>
+      <input
+        id="add-item"
+        value={newItem}
+        onInput={event => setNewItem(event.target.value)}
+        placeholder="Add an item"
+      />
+      <button type="submit" onClick={formSubmit}>Add Item</button>
+    </form>
+  )
+}
