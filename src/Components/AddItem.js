5cb9da514ee31cb7357eaa5715819cbe345d8dcc
Juan Lucero <luceroweb@gmail.com>
2021-12-26 17:28:40 -0800

    feat(blanks): error on empty option string

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -5,18 +5,38 @@ export default function AddItem({ listContainer, setListContainer }) {
 
   const formSubmit = (event) => {
     event.preventDefault();
-    setListContainer([...listContainer, newItem]);
+    if( isBlank(newItem) ) return setNewItem('');
+    setListContainer([...listContainer, newItem.trim()]);
     setNewItem('');
   }
 
+  const isBlank = (option) => {
+    if(option.trim() === '') {
+      document.getElementById('newOption').classList.add("invalid");
+      document.getElementById('newOption').placeholder = 'Option must not be blank';
+      return true;
+    }
+    document.getElementById('newOption').classList.remove("invalid");
+    document.getElementById('newOption').placeholder = 'Add an option';
+    return false;
+  }
+
+  const checkNewItem = (event) => {
+    setNewItem(event.target.value);
+    isBlank(event.target.value.trim());
+  }
+
   return (
-    <form id="add-option">
+    <form id="add-option" onSubmit={formSubmit}>
       <input
+        id="newOption"
+        className=""
         value={newItem}
-        onInput={event => setNewItem(event.target.value)}
+        onInput={event => checkNewItem(event)}
         placeholder="Add an option"
+        required
       />
-      <button type="submit" onClick={formSubmit}>Add Item</button>
+      <button type="submit">Add Item</button>
     </form>
   )
 }
