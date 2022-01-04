a2b51d64098c0cf5d0535b3bebebf5804b740aa2
Juan Lucero <jlucero@alhpaworks.tech>
2022-01-04 12:45:38 -0800

    feat(localStorage): modify array in localStorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import decidr from './decidr.svg';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import AddItem from './Components/AddItem';
 import DisplayList from './Components/DisplayList';
 import GlobalActions from './Components/GlobalActions';
@@ -8,6 +8,22 @@ import GlobalActions from './Components/GlobalActions';
 function App() {
   const [listContainer, setListContainer] = useState([]);
   const [selectedItem, setSelectedItem] = useState('');
+
+  useEffect(
+    () => {
+      setListContainer(
+        JSON.parse(
+          window.localStorage.getItem("listContainer") || "[]"
+        )
+      );
+    },
+    []
+  );
+
+  const updateLocalStorage = (arr) => {
+    window.localStorage.setItem("listContainer",JSON.stringify(arr));
+    setListContainer(arr);
+  }
   
   return (
     <div className="App">
@@ -20,18 +36,18 @@ function App() {
           ? <p id="selected">{selectedItem}</p>
           : <DisplayList
             listContainer={listContainer}
-            setListContainer={setListContainer}
+            setListContainer={updateLocalStorage}
           />
         }
       </main>
       <AddItem
         listContainer={listContainer}
-        setListContainer={setListContainer}
+        setListContainer={updateLocalStorage}
       />
       <footer>
         <GlobalActions
           listContainer={listContainer}
-          setListContainer={setListContainer}
+          setListContainer={updateLocalStorage}
           selectedItem={selectedItem}
           setSelectedItem={setSelectedItem}
         />
